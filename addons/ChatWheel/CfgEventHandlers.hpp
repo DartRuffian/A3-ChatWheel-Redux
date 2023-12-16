4352e31409a0995fad3ecd80e0a37c6e876bad70
DartRuffian <skulldude327@gmail.com>
2023-12-15 20:03:16 -0600

    XEH preInit/preStart

--- addons/ChatWheel/CfgEventHandlers.hpp
+++ addons/ChatWheel/CfgEventHandlers.hpp
@@ -1,31 +1,47 @@
-class Extended_PreInit_EventHandlers
+class Extended_PreStart_EventHandlers
 {
-    class CWR_ConfigureKeybinds
+    class ADDON
     {
-        init = QUOTE(call CWR_fnc_configureAddonKeybinds;);
-    };
-    class CWR_ConfigureOptions
-    {
-        init = QUOTE(call CWR_fnc_configureAddonOptions;);
-    };
-    class CWR_ConfigureChatMenus
-    {
-        init = QUOTE(call CWR_fnc_configureMenus;);
+        init = QUOTE(call COMPILE_SCRIPT(XEH_preStart));
     };
 };
 
-class Extended_PostInit_EventHandlers
+class Extended_PreInit_EventHandlers
 {
-    class CWR_CreateTagDiary
+    class ADDON
     {
-        init = QUOTE(call CWR_fnc_createTagMenu;);
-    };
-    class CWR_ThrowGrenadeEH
-    {
-        init = QUOTE(if isClass (configFile >> 'CfgPatches' >> 'ace_common') then { call CWR_fnc_throwGrenadeEHACE; } else { call CWR_fnc_throwGrenadeEH; };);
-    };
-    class CWR_UnconciousEH
-    {
-        init = QUOTE(call CWR_fnc_unconsciousEH);
+        init = QUOTE(call COMPILE_SCRIPT(XEH_preInit));
     };
 };
+
+// class Extended_PreInit_EventHandlers
+// {
+//     class CWR_ConfigureKeybinds
+//     {
+//         init = QUOTE(call CWR_fnc_configureAddonKeybinds;);
+//     };
+//     class CWR_ConfigureOptions
+//     {
+//         init = QUOTE(call CWR_fnc_configureAddonOptions;);
+//     };
+//     class CWR_ConfigureChatMenus
+//     {
+//         init = QUOTE(call CWR_fnc_configureMenus;);
+//     };
+// };
+
+// class Extended_PostInit_EventHandlers
+// {
+//     class CWR_CreateTagDiary
+//     {
+//         init = QUOTE(call CWR_fnc_createTagMenu;);
+//     };
+//     class CWR_ThrowGrenadeEH
+//     {
+//         init = QUOTE(if isClass (configFile >> 'CfgPatches' >> 'ace_common') then { call CWR_fnc_throwGrenadeEHACE; } else { call CWR_fnc_throwGrenadeEH; };);
+//     };
+//     class CWR_UnconciousEH
+//     {
+//         init = QUOTE(call CWR_fnc_unconsciousEH);
+//     };
+// };
