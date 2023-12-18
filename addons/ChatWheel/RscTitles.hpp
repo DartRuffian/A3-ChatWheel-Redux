46816fa26d0cacd5d485baad972accdefae595f8
DartRuffian <skulldude327@gmail.com>
2023-12-18 00:50:03 -0600

    Basic ListBox HUD

--- addons/ChatWheel/RscTitles.hpp
+++ addons/ChatWheel/RscTitles.hpp
@@ -1,4 +1,6 @@
 #include "\a3\ui_f\hpp\defineCommonGrids.inc"
+#include "\a3\ui_f\hpp\defineCommonColors.inc"
+#include "ui\BaseControls.hpp"
 
 class RscTitles
 {
--- addons/ChatWheel/ui/RscChatWheel.hpp
+++ addons/ChatWheel/ui/RscChatWheel.hpp
@@ -1,10 +1,21 @@
-class RscChatWheel
+class CLASS(RscChatWheel)
 {
     idd = IDD_CHATWHEEL_MAIN;
-    class ControlsBackground
-    {
-    };
+    duration = 1e+6;
+    onLoad = "uiNamespace setVariable ['CWR_RscChatWheel', _this select 0];";
+
     class Controls
     {
+        class Messages: RscListBox
+        {
+            idc = IDC_CHATWHEEL_MESSAGES;
+            x = __EVAL(GUI_GRID_CENTER_X);
+            y = __EVAL(GUI_GRID_CENTER_Y);
+            w = __EVAL(5 * GUI_GRID_CENTER_W);
+            h = __EVAL(10 * GUI_GRID_CENTER_H);
+
+            colorText[] = IGUI_TEXT_COLOR;
+            colorBackground[] = IGUI_BCG_COLOR;
+        };
     };
 };
