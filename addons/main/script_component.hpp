e9e7d3b6409d8cbe4a5f34eafbe45ef83e2a47b3
DartRuffian <skulldude327@gmail.com>
2024-01-16 13:20:00 -0600

    PREP recompile

--- addons/main/script_component.hpp
+++ addons/main/script_component.hpp
@@ -3,6 +3,6 @@
 #include "\DA\CWR\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
+#define DISABLE_COMPILE_CACHE
 
 #include "\DA\CWR\addons\main\script_macros.hpp"
