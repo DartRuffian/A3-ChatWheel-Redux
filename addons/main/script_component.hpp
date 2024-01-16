eb2cf484a90bb85d5183e648c719a1300878aafb
DartRuffian <skulldude327@gmail.com>
2024-01-16 13:12:24 -0600

    Updated script components

--- addons/main/script_component.hpp
+++ addons/main/script_component.hpp
@@ -1,5 +1,8 @@
 #define COMPONENT main
 #define COMPONENT_BEAUTIFIED Main
-
 #include "\DA\CWR\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
 #include "\DA\CWR\addons\main\script_macros.hpp"
