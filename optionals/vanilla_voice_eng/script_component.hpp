713623f84b36a184d93212d825aeee7ae5118286
DartRuffian <skulldude327@gmail.com>
2024-01-16 21:35:56 -0600

    Renamed vanilla_voice to vanilla_voice_eng

--- optionals/vanilla_voice_eng/script_component.hpp
+++ optionals/vanilla_voice_eng/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT vanilla_voice
-#define COMPONENT_BEAUTIFIED Vanilla Voice
+#define COMPONENT vanilla_voice_eng
+#define COMPONENT_BEAUTIFIED Vanilla Voice - English
 
 #include "\DA\CWR\addons\main\script_mod.hpp"
 #include "\DA\CWR\addons\main\script_macros.hpp"
