170fae4fd325be93eea329c1680f7e44401cd62a
DartRuffian <skulldude327@gmail.com>
2024-02-09 12:13:39 -0600

    Fixed quote error

--- addons/chatwheel/CWR_ChatWheel_Tags.hpp
+++ addons/chatwheel/CWR_ChatWheel_Tags.hpp
@@ -33,19 +33,19 @@ class CWR_ChatWheel_Tags
             class Infantry
             {
                 displayName = CSTRING(tagTitle_Contact_Infantry);
-                statement = QUOTE(LLSTRING(tag_Contact_Infantry));
+                statement = QUOTE(QLLSTRING(tag_Contact_Infantry));
                 order = 0;
             };
             class Vehicle
             {
                 displayName = CSTRING(tagTitle_Contact_Vehicle);
-                statement = QUOTE(LLSTRING(tag_Contact_Vehicle));
+                statement = QUOTE(QLLSTRING(tag_Contact_Vehicle));
                 order = 1;
             };
             class Fortification
             {
                 displayName = CSTRING(tagTitle_Contact_Fortification);
-                statement = QUOTE(LLSTRING(tag_Contact_Fortification));
+                statement = QUOTE(QLLSTRING(tag_Contact_Fortification));
                 order = 2;
             };
         };
@@ -62,19 +62,19 @@ class CWR_ChatWheel_Tags
             class Close
             {
                 displayName = CSTRING(tagTitle_Distance_Close);
-                statement = QUOTE(LLSTRING(tag_Distance_Close));
+                statement = QUOTE(QLLSTRING(tag_Distance_Close));
                 order = 0;
             };
             class Medium
             {
                 displayName = CSTRING(tagTitle_Distance_Medium);
-                statement = QUOTE(LLSTRING(tag_Distance_Medium));
+                statement = QUOTE(QLLSTRING(tag_Distance_Medium));
                 order = 1;
             };
             class Far
             {
                 displayName = CSTRING(tagTitle_Distance_Far);
-                statement = QUOTE(LLSTRING(tag_Distance_Far));
+                statement = QUOTE(QLLSTRING(tag_Distance_Far));
                 order = 2;
             };
         };
@@ -91,31 +91,31 @@ class CWR_ChatWheel_Tags
             class CallCheck
             {
                 displayName = CSTRING(tagTitle_Status_CallCheck);
-                statement = QUOTE(LLSTRING(tag_Status_CallCheck));
+                statement = QUOTE(QLLSTRING(tag_Status_CallCheck));
                 order = 0;
             };
             class Green
             {
                 displayName = CSTRING(tagTitle_Status_Green);
-                statement = QUOTE(LLSTRING(tag_Status_Green));
+                statement = QUOTE(QLLSTRING(tag_Status_Green));
                 order = 1;
             };
             class Yellow
             {
                 displayName = CSTRING(tagTitle_Status_Yellow);
-                statement = QUOTE(LLSTRING(tag_Status_Yellow));
+                statement = QUOTE(QLLSTRING(tag_Status_Yellow));
                 order = 2;
             };
             class Red
             {
                 displayName = CSTRING(tagTitle_Status_Red);
-                statement = QUOTE(LLSTRING(tag_Status_Red));
+                statement = QUOTE(QLLSTRING(tag_Status_Red));
                 order = 3;
             };
             class Black
             {
                 displayName = CSTRING(tagTitle_Status_Black);
-                statement = QUOTE(LLSTRING(tag_Status_Black));
+                statement = QUOTE(QLLSTRING(tag_Status_Black));
                 order = 4;
             };
         };
@@ -132,25 +132,25 @@ class CWR_ChatWheel_Tags
             class AskPermission
             {
                 displayName = CSTRING(tagTitle_Launcher_AskPermission);
-                statement = QUOTE(LLSTRING(tag_Launcher_AskPermission));
+                statement = QUOTE(QLLSTRING(tag_Launcher_AskPermission));
                 order = 0;
             };
             class ClearBackblast
             {
                 displayName = CSTRING(tagTitle_Launcher_ClearBackblast);
-                statement = QUOTE(LLSTRING(tag_Launcher_ClearBackblast));
+                statement = QUOTE(QLLSTRING(tag_Launcher_ClearBackblast));
                 order = 1;
             };
             class BackblastClear
             {
                 displayName = CSTRING(tagTitle_Launcher_BackblastClear);
-                statement = QUOTE(LLSTRING(tag_Launcher_BackblastClear));
+                statement = QUOTE(QLLSTRING(tag_Launcher_BackblastClear));
                 order = 2;
             };
             class FireRocket
             {
                 displayName = CSTRING(tagTitle_Launcher_FireRocket);
-                statement = QUOTE(LLSTRING(tag_Launcher_FireRocket));
+                statement = QUOTE(QLLSTRING(tag_Launcher_FireRocket));
                 order = 3;
             };
         };
