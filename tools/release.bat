aac925421190e17ba1a8c96fe0a75bc2f9dd276c
DartRuffian <skulldude327@gmail.com>
2024-10-12 01:50:14 -0500

    Update readme

--- tools/release.bat
+++ tools/release.bat
@@ -1,5 +1,4 @@
 @ECHO off
-winget install --id=BrettMayson.HEMTT  -e
 hemtt script update_build.rhai
 hemtt script update_minor.rhai
 hemtt release
--- tools/release_patch.bat
+++ tools/release_patch.bat
@@ -1,5 +1,4 @@
 @ECHO off
-winget install --id=BrettMayson.HEMTT  -e
 hemtt script update_build.rhai
 hemtt script update_patch.rhai
 hemtt release
